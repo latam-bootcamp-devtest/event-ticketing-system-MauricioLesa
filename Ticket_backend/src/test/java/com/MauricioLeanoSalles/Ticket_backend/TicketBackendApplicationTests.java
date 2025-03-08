e346a03c8dfbdb93020f9a296a88f8725d03e2ab
Mauricio Leaño Salles <maulsalles@gmail.com>
2025-03-08 09:17:52 -0400

    starting backend and frontend

---
+++ Ticket_backend/src/test/java/com/MauricioLeanoSalles/Ticket_backend/TicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MauricioLeanoSalles.Ticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
